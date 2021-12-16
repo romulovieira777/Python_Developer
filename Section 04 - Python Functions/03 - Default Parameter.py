9fe49fa0b41d48408c4d0d41fc18050a60160a6a
romulovieira777 <romulo.vieira777@gmail.com>
2021-12-16 11:40:19 -0300

    Python Functions

---
+++ Section 04 - Python Functions/03 - Default Parameter.py
@@ -0,0 +1,7 @@
+def student_names(names="Bluelime"):
+    print("Hello " + names)
+
+
+student_names()
+student_names("John")
+student_names("Jane")
---
+++ Section 04 - Python Functions/04 - Keyword Arguments.py
@@ -0,0 +1,7 @@
+def more_num(a, b=7, c=10):
+    print("a is ", a, "and b is ", b, "while c is ", c)
+
+
+more_num(3, 7)
+more_num(23, c=17)
+more_num(c=40, a=80)
