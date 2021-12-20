8fae5c49224ce777b745a611273b28c940ceca45
romulovieira777 <romulo.vieira777@gmail.com>
2021-12-20 16:48:41 -0300

    Python Data Structures

--- Section 06 - Python Data Structures/01 - Create List.py
+++ Section 06 - Python Data Structures/01 - Create List.py
@@ -3,8 +3,3 @@ animals = ["Bear", "Tiger", "Lion", "Panda", "Elephant"]
 for x in animals:
     print(x)
 print(animals)
-
-
-
-
-
---
+++ Section 06 - Python Data Structures/02 - Accessing Elements.py
@@ -0,0 +1,9 @@
+animals = ["Bear", "Tiger", "Lion", "Panda", "Elephant"]
+
+print(animals[0])
+print(animals[-1])
+print(animals[1:])
+print(animals[1:3])
+
+animals[0] = "Dog"
+print(animals[0])
