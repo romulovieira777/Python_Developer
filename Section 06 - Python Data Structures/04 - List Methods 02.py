8d126adbfc33f247603e744bef53ce088acddb27
romulovieira777 <romulo.vieira777@gmail.com>
2021-12-22 20:41:02 -0300

    Python Data Structures

---
+++ Section 06 - Python Data Structures/04 - List Methods 02.py
@@ -0,0 +1,20 @@
+fruits = ["Berries", "Apples", "Grapes", "Oranges"]
+vegetables = ["Kale", "Broccoli", "Lettuce"]
+
+# Method index
+print(fruits.index('Grapes'))
+
+# Method insert
+fruits.insert(0, "Banana")
+print(fruits)
+
+# Method pop
+fruits.pop()
+print(fruits)
+
+fruits.pop(1)
+print(fruits)
+
+# Method remove
+vegetables.remove("Kale")
+print(vegetables)
