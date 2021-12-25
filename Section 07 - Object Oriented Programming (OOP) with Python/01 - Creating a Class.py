773b30bb2b060d67ad93acd4f2bb209d343e4e92
romulovieira777 <romulo.vieira777@gmail.com>
2021-12-25 12:17:24 -0300

    Object Oriented Programming (OOP) with Python

---
+++ Section 07 - Object Oriented Programming (OOP) with Python/01 - Creating a Class.py
@@ -0,0 +1,8 @@
+class Instructors:
+    companyName = "Bluelime"
+
+    def __init__(self, course):
+        self.course = course
+
+    def printinfo(self):
+        print("My Company name is ", Instructors.companyName)
