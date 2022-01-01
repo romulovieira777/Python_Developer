86a441a142a0ba2dec8a3b878a01c01162feb644
romulovieira777 <romulo.vieira777@gmail.com>
2022-01-01 12:07:34 -0300

    Object Oriented Programming (OOP) with Python

---
+++ Section 07 - Object Oriented Programming (OOP) with Python/07 - Inheritance 04.py
@@ -0,0 +1,29 @@
+class Person:
+    def __init__(self, fname, lname):
+        self.firstname = fname
+        self.lastname = lname
+
+    def printname(self):
+        print(self.firstname, self.lastname)
+
+
+florist = Person("Jane", "Flowers")
+florist.printname()
+
+
+class Lawyers(Person):
+    def __init__(self, fname, lname, casetype):
+        Person.__init__(self, fname, lname)
+        self.casetype = casetype
+
+        # self.firstname = fname
+        # self.lastname = lname
+
+    def printinfo(self):
+        print("Hello my name is", self.firstname, self.lastname)
+
+
+happy_lawyers = Lawyers("Jack", "Smiley", "Criminal")
+happy_lawyers.printinfo()
+
+print(happy_lawyers.casetype)
